177cb7c60f6333c0c83e65456c63c8745527d26c
Jared Hall <hall.jared.c@gmail.com>
2023-11-12 13:11:39 -0600

    added a silly index file to make sure things work. resolve-tspaths seems to fix the problem

---
+++ index.js
@@ -0,0 +1,13 @@
+import {Game} from "./dist/src/Game/Game.js";
+import {PgnFile} from "./dist/src/PgnFile/PgnFile.js";
+
+console.log("hi")
+
+const game = Game.makeNewGame();
+game.setInputType('Coordinate')
+game.makeMove('e2e4');
+game.makeMove('e7e5');
+game.setResigns('white');
+
+const file = PgnFile.make(game)
+console.log(file.content);
